1399bf041591e6afb51310c7e23b1c0884d61b77
Marcelo Parabocz <macerno@gmail.com>
2026-02-17 14:38:43 -0300

    refactor: introduce domain exceptions and handle API errors properly

---
+++ src/main/java/com/aracnideo/exception/CardNotFoundException.java
@@ -0,0 +1,10 @@
+package com.aracnideo.exception;
+
+public class CardNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public CardNotFoundException(String message) {
+		super(message);
+	}
+}
