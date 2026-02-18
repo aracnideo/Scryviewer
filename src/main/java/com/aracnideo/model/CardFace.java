e9a69154f8f1dba73cb405fb0a134038ff2e9de5
Marcelo Parabocz <macerno@gmail.com>
2026-02-18 10:38:06 -0300

    refactor: support for double-faced cards

---
+++ src/main/java/com/aracnideo/model/CardFace.java
@@ -0,0 +1,114 @@
+package com.aracnideo.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CardFace implements CardSide {
+
+	@JsonProperty("name")
+	private String name;
+	@JsonProperty("mana_cost")
+	private String manaCost;
+	@JsonProperty("type_line")
+	private String typeLine;
+	@JsonProperty("oracle_text")
+	private String oracleText;
+	@JsonProperty("power")
+	private String power;
+	@JsonProperty("toughness")
+	private String toughness;
+	@JsonProperty("flavor_text")
+	private String flavorText;
+	@JsonProperty("image_uris")
+	private ImageUris imageUris;
+
+	public CardFace() {
+
+	}
+
+	public CardFace(String name, String manaCost, String typeLine, String oracleText, String power, String toughness,
+			String flavorText, ImageUris imageUris) {
+		super();
+		this.name = name;
+		this.manaCost = manaCost;
+		this.typeLine = typeLine;
+		this.oracleText = oracleText;
+		this.power = power;
+		this.toughness = toughness;
+		this.flavorText = flavorText;
+		this.imageUris = imageUris;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getManaCost() {
+		return manaCost;
+	}
+
+	public void setManaCost(String manaCost) {
+		this.manaCost = manaCost;
+	}
+
+	public String getTypeLine() {
+		return typeLine;
+	}
+
+	public void setTypeLine(String typeLine) {
+		this.typeLine = typeLine;
+	}
+
+	public String getOracleText() {
+		return oracleText;
+	}
+
+	public void setOracleText(String oracleText) {
+		this.oracleText = oracleText;
+	}
+
+	public String getPower() {
+		return power;
+	}
+
+	public void setPower(String power) {
+		this.power = power;
+	}
+
+	public String getToughness() {
+		return toughness;
+	}
+
+	public void setToughness(String toughness) {
+		this.toughness = toughness;
+	}
+
+	public String getFlavorText() {
+		return flavorText;
+	}
+
+	public void setFlavorText(String flavorText) {
+		this.flavorText = flavorText;
+	}
+
+	public ImageUris getImageUris() {
+		return imageUris;
+	}
+
+	public void setImageUris(ImageUris imageUris) {
+		this.imageUris = imageUris;
+	}
+
+	@Override
+	public String toString() {
+		return "CardFace [name=" + name + ", manaCost=" + manaCost + ", typeLine=" + typeLine + ", oracleText="
+				+ oracleText + ", power=" + power + ", toughness=" + toughness + ", flavorText=" + flavorText
+				+ ", imageUris=" + imageUris + "]";
+	}
+
+}
---
+++ src/main/java/com/aracnideo/model/CardSide.java
@@ -0,0 +1,13 @@
+package com.aracnideo.model;
+
+public interface CardSide {
+
+	String getName();
+	String getManaCost();
+	String getTypeLine();
+	String getOracleText();
+	String getPower();
+	String getToughness();
+	String getFlavorText();
+	ImageUris getImageUris();
+}
