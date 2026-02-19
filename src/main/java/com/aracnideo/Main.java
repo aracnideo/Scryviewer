a37cdffb0af3bdd9f72d69da6eaddaba2234ccb0
Marcelo Parabocz <macerno@gmail.com>
2026-02-19 10:14:21 -0300

    refactor: remove unnecessary throws

--- src/main/java/com/aracnideo/Main.java
+++ src/main/java/com/aracnideo/Main.java
@@ -2,13 +2,9 @@ package com.aracnideo;
 
 import com.aracnideo.view.CardViewer;
 
-
-//import com.aracnideo.model.Card;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-
 public class Main {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 
 		new CardViewer();
 	}
